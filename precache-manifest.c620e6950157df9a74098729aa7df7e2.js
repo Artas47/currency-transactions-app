526aeba949c0cef6cb5eb1ddf1136a0a8437de9e
artas <artasss47@gmail.com>
2020-06-02 01:01:39 +0200

    Updates

--- precache-manifest.c620e6950157df9a74098729aa7df7e2.js
+++ precache-manifest.c620e6950157df9a74098729aa7df7e2.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e6ed92aabaeaf0cea7ee76a73041a4c1",
+    "revision": "eb0a1d6a27ba412c8800c344d1d507a4",
     "url": "/currency-transactions-app/index.html"
   },
   {
-    "revision": "332c22bc6d7a013bfca6",
+    "revision": "28bf7c9db85e08785dfd",
     "url": "/currency-transactions-app/static/css/main.3178ae8d.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/currency-transactions-app/static/js/2.bc767a52.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "332c22bc6d7a013bfca6",
-    "url": "/currency-transactions-app/static/js/main.cf7d2a5c.chunk.js"
+    "revision": "28bf7c9db85e08785dfd",
+    "url": "/currency-transactions-app/static/js/main.41a6591f.chunk.js"
   },
   {
     "revision": "e1e7b7c614310c71c32a",
