06f0085da4908c0b67886257ad5038fad5e661e4
artas <artasss47@gmail.com>
2020-06-02 01:33:50 +0200

    moved routing to app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import React from 'react'
 import './App.css'
-import {Link} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
 import TransactionsList from './components/transactions-list/transactions-list'
 import TopTransaction from './components/top-transaction/top-transaction'
 import Predictor from './components/predictor/predictor'
 import * as Styles from './App.styles'
 import CustomButton from './components/custom-button/custom-button'
+import AddTransactionForm from './components/add-transaction-form/add-transaction-form'
 
 const App = () => {
   return (
     <Styles.AppContainer>
-      <TransactionsList />
-      <Styles.SideBar>
-        <Predictor />
-        <Link style={{textDecoration: 'none'}} to="addTransaction">
-          <CustomButton add>Add Transaction</CustomButton>
-        </Link>
-        <TopTransaction />
-      </Styles.SideBar>
+      <Router>
+        <Route path="/">
+          <TransactionsList />
+          <Styles.SideBar>
+            <Predictor />
+            <Link style={{textDecoration: 'none'}} to="addTransaction">
+              <CustomButton add>Add Transaction</CustomButton>
+            </Link>
+            <TopTransaction />
+          </Styles.SideBar>
+        </Route>
+        <Route path="/addTransaction">
+          <AddTransactionForm />
+        </Route>
+      </Router>
     </Styles.AppContainer>
   )
 }
--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import {Provider} from 'react-redux'
 import {createStore, applyMiddleware, compose} from 'redux'
-import {BrowserRouter, Route} from 'react-router-dom'
 import App from './App'
-import AddTransactionForm from './components/add-transaction-form/add-transaction-form'
 import reducers from './reducers/index'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
@@ -12,14 +10,7 @@ const store = createStore(reducers, composeEnhancers(applyMiddleware()))
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
-      <Route path="/">
-        <App />
-      </Route>
-      <Route path="/addTransaction">
-        <AddTransactionForm />
-      </Route>
-    </BrowserRouter>
+    <App />
   </Provider>,
   document.getElementById('root'),
 )
